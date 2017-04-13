49780bb7845c9087c19453458ab10de92a35a14e
vathsav <vathsav@live.in>
2017-04-13 14:59:30 +0200

    temp: incomplete push

--- js/faculties.js
+++ js/faculties.js
@@ -88,12 +88,16 @@ let engineering = [
 
 let facultyNodes = {};
 let keywords = [];
-let radius = (window.innerWidth / 2) * .5;
+let radius = (window.innerWidth / 2) * .4;
 let size = null;
 let total = 0;
 
+var page = ["home", "departments", "keyword"];
+var currentPage = page[0];
+
 // Fetch data
-firebase.database().ref('/keywords').orderByChild('total').startAt(30).once('value', snapshot => {
+// 54325 keywords
+firebase.database().ref('/keywords').orderByChild('total').startAt(15).once('value', snapshot => {
   snapshot.forEach(keyword => {
     var word = keyword.val();
     var final = {};
@@ -118,12 +122,8 @@ firebase.database().ref('/keywords').orderByChild('total').startAt(30).once('val
 
     final.keyword = keyword.key;
     final.total = word.total;
-
-    if (word.total > 30) {
-      keywords.push(final);
-      // Calculate total number of keywords
-      total += final.total;
-    }
+    keywords.push(final);
+    total += final.total;
   });
 }).then(v => {
   size = d3.scaleSqrt()
@@ -154,24 +154,21 @@ firebase.database().ref('/keywords').orderByChild('total').startAt(30).once('val
       colorGreen += multiplier * d3.color(colors[faculty]).g;
       colorBlue += multiplier * d3.color(colors[faculty]).b;
       down += multiplier;
-     });
+    });
 
     keyword.positionX = upX / down;
     keyword.positionY = upY / down;
-
-    // Test RGB
-    // colorRed = 133;
-    // colorGreen = 120;
-    // colorBlue = 0;
 
     let color = d3.rgb(colorRed, colorGreen, colorBlue);
     let lightness = 1 / Math.sqrt(Math.pow(keyword.positionX - center[0], 2) + Math.pow(keyword.positionY - center[1], 2));
     keyword.color = color.brighter(lightness / 0.0080);
   });
 
+  var bubbles = keywords;
+
   _.forEach(facultyNodes, function(value, key) {
     // Push the faculty nodes into the set of keywords (so that they can be mapped together)
-    keywords.push({
+    bubbles.push({
       keyword: key,
       type: "focus",
       positionX: value[0],
@@ -181,14 +178,26 @@ firebase.database().ref('/keywords').orderByChild('total').startAt(30).once('val
     });
   });
 
-  var bubbles = keywords;
+  var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function(bubble) {
+      if (faculties.includes(bubble.keyword)) {
+        var department;
+        if (bubble.keyword == 'a') department = 'Architecture';
+        else if (bubble.keyword == 'd') department = 'Design';
+        else if (bubble.keyword == 'e') department = 'Engineering';
+        return "<strong style='color: #E0E0E0;'>Department: </strong> <span style='color:red'>" + department + "</span>";
+      }
+      return "<strong style='color: #E0E0E0;'>Word:</strong> <span style='color:red'>" + bubble.keyword + "</span> <br> <strong style='color: #E0E0E0;'>Architecture:</strong> <span style='color:red'>" + bubble.a + "</span> <br> <strong style='color: #E0E0E0;'>Design:</strong> <span style='color:red'>" + bubble.d + "</span> <br> <strong style='color: #E0E0E0;'>Engineering:</strong> <span style='color:red'>" + bubble.e + "</span>";
+    });
 
   var simulation = d3.forceSimulation()
     .force("x", d3.forceX().strength(.2).x(function(bubble) {
-        return bubble.positionX;
+      return bubble.positionX;
     }))
     .force("y", d3.forceY().strength(.2).y(function(bubble) {
-        return bubble.positionY;
+      return bubble.positionY;
     }))
     .force("charge", d3.forceManyBody().strength(-1))
     .force("collide", d3.forceCollide().radius(function(bubble) {
@@ -196,26 +205,27 @@ firebase.database().ref('/keywords').orderByChild('total').startAt(30).once('val
     }).iterations(2))
     .force("center", d3.forceCenter(window.innerWidth / 2, window.innerHeight / 2 - 20));
 
-  var tip = d3.tip()
-    .attr('class', 'd3-tip')
-    .offset([-10, 0])
-    .html(function(bubble) {
-      return "<strong style='color: #E0E0E0;'>Word:</strong> <span style='color:red'>" + bubble.keyword + "</span> <br> <strong style='color: #E0E0E0;'>Architecture:</strong> <span style='color:red'>" + bubble.a + "</span> <br> <strong style='color: #E0E0E0;'>Design:</strong> <span style='color:red'>" + bubble.d + "</span> <br> <strong style='color: #E0E0E0;'>Engineering:</strong> <span style='color:red'>" + bubble.e + "</span>";
-    })
-
-
   simulation
-    .nodes(keywords)
+    .nodes(bubbles)
     .on("tick", ticked);
 
-  let svg = d3.select("body").append("svg")
+  simulation.stop();
+
+  function ticked() {
+    bubble.attr("cx", function(bubble) {
+      return bubble.x;
+    }).attr("cy", function(bubble) {
+      return bubble.y;
+    });
+  }
+
+  var svg = d3.select(".visualisation").append("svg")
     .attr("width", window.innerWidth)
-    .attr("height", window.innerWidth)
+    .attr("height", window.innerHeight)
     .call(d3.zoom()
       .scaleExtent([1 / 2, 4])
       .on("zoom", zoomed)
     )
-    .call(tip);
 
   var g = svg.append("g")
     .attr("class", "g")
@@ -243,133 +253,207 @@ firebase.database().ref('/keywords').orderByChild('total').startAt(30).once('val
     .style("fill", function(bubble) {
       return bubble.color;
     })
-    .on('click', clicked)
-    .on('mouseenter', mouseEnter)
-    .on('mouseleave', mouseLeave);
+    .on('click', function(d) {
+      currentPage = page[2];
 
-    function clicked(bubble, index) {
-      $("#sidebar").show();
-      document.getElementById("keyword").innerHTML = bubble.keyword;
-      document.getElementById("architecture").innerHTML = "Architecture: " + bubble.a;
-      document.getElementById("design").innerHTML = "Design: " + bubble.d;
-      document.getElementById("engineering").innerHTML = "Engineering: " + bubble.e;
-    }
+      if (!faculties.includes(bubble.keyword)) {
+        $("#back").show();
+        d3.selectAll(".keyword").style("opacity", 0);
+        tip.attr('class', '')
+          .html('');
+      }
 
-    function mouseEnter(bubble, index) {
-      if (!faculties.includes(bubble.keyword))
-        tip.show(bubble, index);
-    }
+      exploreKeyword(bubble.keyword);
+    })
+    .on('mouseenter', tip.show)
+    .on('mouseleave', tip.hide);
 
-    function mouseLeave(bubble, index) {
-      tip.hide(bubble, index);
-    }
+  showHome();
 
-    // .style("stroke", function(d, i) {
-    //     return d.type ? colors[d.keyword] : "none"
-    // })
-    // .style("stroke-width", 5)
-    // .style("totality", .9)
-    // .on("mouseenter", mouseEnter)
-    // .on("mouseleave", mouseLeave);
+  function showHome() {
+    $('#title').show();
+    $('#intro').show();
+    $('#credits').show();
+    $('#back').hide();
 
-  // bubble.filter(function(d) {
-  //         return d.type;
-  //     })
-  //     .attr("id", function(d) {
-  //         return d.keyword.replace(/\&/g, "")
-  //     });
-
-  // var tooltip = g.selectAll(".keyword-txt").data(bubbles)
-  //     .enter().append("text")
-  //     .attr("class", function(d) {
-  //         return d.type ? "keyword-txt keyword-attractor" : "keyword-txt k" + d.keyword.replace(/ /g, "_")
-  //     })
-  //     .attr("text-anchor", function(d) {
-  //         return d.type ? "middle" : "left";
-  //     })
-  //     .attr("dy", 6)
-  //     .style("opacity", function(d) {
-  //         return size(d.total) > 8 ? 1 : 0
-  //     })
-  //     .style("fill", function(d) {
-  //         return d.type ? "#040404" : "#fcfff7";
-  //     })
-  //     .text(function(d) {
-  //         return d.keyword;
-  //     })
-  //
-  // tooltip.filter(function(d) {
-  //         return d.type
-  //     })
-  //     .attr("id", function(d) {
-  //         return "txt_" + d.keyword.replace(/\&/g, "");
-  //     })
-
-  // function mouseEnter(d) {
-  //     mouseLeave();
-  //
-  //     var id = d3.select(this).attr("id")
-  //
-  //     if (id) {
-  //
-  //         d3.selectAll(".keyword:not([id])").filter(function(e) {
-  //             return e[id] > 0
-  //         }).style("opacity", 1)
-  //
-  //     } else {
-  //         console.log(d);
-  //
-  //         d3.selectAll(".keyword-txt:not([id])").style("opacity", 0)
-  //         d3.select(".k" + d.keyword.replace(/ /g, "_")).style("opacity", 1)
-  //
-  //         _.forEach(d, function(v, k) {
-  //             if (v > 0 && k in faculties) {
-  //
-  //                 $(".info-cats").append("<div style='color:" + cols[k] + "'>" + k + ": <span>" + v + "</span></div>");
-  //
-  //                 sel.append("line")
-  //                     .attr("x1", d3.select("#" + k.replace(/\&/g, "")).attr("cx"))
-  //                     .attr("x2", d.x)
-  //                     .attr("y1", d3.select("#" + k.replace(/\&/g, "")).attr("cy"))
-  //                     .attr("y2", d.y)
-  //                     .style("stroke", "#fcfff7")
-  //                     .style("stroke-width", function(d) {
-  //                         return strscale(v)
-  //                     });
-  //             }
-  //         })
-  //         $(".curr-key h1").text(d.keyword)
-  //         $(".curr-key").show();
-  //     }
-  //
-  //     d3.selectAll(".keyword:not([id])").style("opacity", .3)
-  //     d3.select(this).style("opacity", 1)
-  // }
-  //
-  // function mouseLeave(d) {
-  //     d3.selectAll(".keyword").style("opacity", .9)
-  //     d3.selectAll(".keyword-txt").style("opacity", function(d) {
-  //             return size(d.total) > 8 ? 1 : 0
-  //         })
-  //         //d3.select("."+d.keyword.replace(/ /g,"_")).style("opacity",function(d){return siz(d.tot)>8 ? 1 : 0})
-  //     d3.select(".sel").selectAll("*").remove();
-  //     $(".curr-key").hide();
-  //     $(".info-cats").empty();
-  // }
-
-  // Tincy rotation
-  var speed = 0;
-  // d3.timer(function() {
-  //   svg.fill
-  //   svg.style("transform", "rotate(" + speed + "deg)");
-  //   speed -= .0125;
-  // });
-
-  function ticked() {
-    bubble.attr("cx", function(bubble) {
-        return bubble.x;
-      }).attr("cy", function(bubble) {
-        return bubble.y;
-      });
+    // Polimi Circle
+    var polimiBubble = svg.append("ellipse")
+      .attr("cx", center[0])
+      .attr("cy", center[1])
+      .attr("rx", 0)
+      .attr("ry", 0)
+      .attr("fill", "#E1BEE7")
+      .on("click", function(d) {
+        $('#title').hide();
+        $('#intro').hide();
+        $('#credits').hide();
+        $('#back').show();
+        d3.select("ellipse").transition()
+          .attr("rx", 0)
+          .attr("ry", 0)
+          .style("fill", "#E1BEE7");
+        currentPage = page[1];
+        showDepartments();
+      })
+      .transition()
+        .attr("rx", (total / size(total)) * 0.5)
+        .attr("ry", (total / size(total)) * 0.5)
+        .style("fill", "#6A1B9A")
   }
+
+  function showDepartments() {
+    currentPage = page[1];
+    simulation.restart();
+    // svg.call(tip);
+
+    // Tincy rotation
+    var speed = 0;
+    // d3.timer(function() {
+    //   svg.fill
+    //   svg.style("transform", "rotate(" + speed + "deg)");
+    //   speed -= .0125;
+    // });
+  }
+
+  function exploreKeyword(keyword) {
+    currentPage = page[2];
+    var units = "Widgets";
+
+    // set the dimensions and margins of the graph
+    var margin = {
+        top: 10,
+        right: 10,
+        bottom: 10,
+        left: 10
+      },
+      width = 700 - margin.left - margin.right,
+      height = 300 - margin.top - margin.bottom;
+
+    // format variables
+    var formatNumber = d3.format(",.0f"), // zero decimal places
+      format = function(d) {
+        return formatNumber(d) + " " + units;
+      },
+      color = d3.scaleOrdinal(d3.schemeCategory20);
+
+    // append the svg object to the body of the page
+    var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+    // Set the sankey diagram properties
+    var sankey = d3.sankey()
+      .nodeWidth(36)
+      .nodePadding(40)
+      .size([width, height]);
+
+    var path = sankey.link();
+
+    // load the data
+    d3.json("js/data.json", function(error, graph) {
+
+      sankey
+        .nodes(graph.nodes)
+        .links(graph.links)
+        .layout(32);
+
+      // add in the links
+      var link = svg.append("g").selectAll(".link")
+        .data(graph.links)
+        .enter().append("path")
+        .attr("class", "link")
+        .attr("d", path)
+        .style("stroke-width", function(d) {
+          return Math.max(1, d.dy);
+        })
+        .sort(function(a, b) {
+          return b.dy - a.dy;
+        });
+
+      // add the link titles
+      link.append("title")
+        .text(function(d) {
+          return d.source.name + " → " +
+            d.target.name + "\n" + format(d.value);
+        });
+
+      // add in the nodes
+      var node = svg.append("g").selectAll(".node")
+        .data(graph.nodes)
+        .enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) {
+          return "translate(" + d.x + "," + d.y + ")";
+        })
+        .call(d3.drag()
+          .subject(function(d) {
+            return d;
+          })
+          .on("start", function() {
+            this.parentNode.appendChild(this);
+          })
+          .on("drag", dragmove));
+
+      // add the rectangles for the nodes
+      node.append("rect")
+        .attr("height", function(d) {
+          return d.dy;
+        })
+        .attr("width", sankey.nodeWidth())
+        .style("fill", function(d) {
+          return d.color = color(d.name.replace(/ .*/, ""));
+        })
+        .style("stroke", function(d) {
+          return d3.rgb(d.color).darker(2);
+        })
+        .append("title")
+        .text(function(d) {
+          return d.name + "\n" + format(d.value);
+        });
+
+      // add in the title for the nodes
+      node.append("text")
+        .attr("x", -6)
+        .attr("y", function(d) {
+          return d.dy / 2;
+        })
+        .attr("dy", ".35em")
+        .attr("text-anchor", "end")
+        .attr("transform", null)
+        .text(function(d) {
+          return d.name;
+        })
+        .filter(function(d) {
+          return d.x < width / 2;
+        })
+        .attr("x", 6 + sankey.nodeWidth())
+        .attr("text-anchor", "start");
+
+      // the function for moving the nodes
+      function dragmove(d) {
+      //   d3.select(this)
+      //     .attr("transform",
+      //       "translate(" +
+      //       d.x + "," +
+      //       (d.y = Math.max(
+      //         0, Math.min(height - d.dy, d3.event.y))) + ")");
+      //   sankey.relayout();
+      //   link.attr("d", path);
+      }
+    });
+  }
+
+  $("#back").click(function(d) {
+    if (currentPage == page[1]) {
+      currentPage = page[0];
+      d3.selectAll(".keyword").style("opacity", 0);
+      showHome();
+    } else if (currentPage == page[2]) {
+      currentPage = page[1];
+      d3.selectAll(".keyword").style("opacity", 1);
+    }
+  });
 });
